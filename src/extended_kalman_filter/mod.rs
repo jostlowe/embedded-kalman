90d21938b1730cec40ec774c710faef1e8b60554
Jostein Løwer <jostlowe@gmail.com>
2022-10-10 15:51:24 +0200

    Trying a more Trait-oriented approach

--- src/extended_kalman_filter/mod.rs
+++ src/extended_kalman_filter/mod.rs
@@ -3,99 +3,41 @@ mod tests;
 use nalgebra::{Matrix2, RealField, SMatrix, SVector, Vector2};
 use crate::covariance_matrix::CovarianceMatrix;
 
-/// A standard linear time-invariant Kalman Filter.
-///
-/// ## Initialization
-/// A new Kalman Filter is created using the [KalmanFilter::new()] function.
-/// The Kalman Filter is generic around 5 parameters
-/// * `T`: The underlying numeric type used in the calculations of the filter. Is usually `f32` or `f64` depending on your system
-/// * `Nx`: The size of your state vector `x`
-/// * `Nz`: The size of your measurement vector `z`
-/// * `Nu`: The size of your input vector `u`
-/// * `S`: The state of the Kalman Filter. This is either `Predict` or `Update`. The filter is always created in the `Update` state
-///
-/// To create an instance of the KalmanFilter you will need:
-/// * A state transition Matrix `A` with dimensions `Nx`*`Nx`
-/// * A state input Matrix `B` with dimensions `Nx`*`Nu`
-/// * A state transition noise covariance Matrix `Q` with dimensions `Nx`*`Nx`
-/// * A measurement noise covariance Matrix `R` with dimensions `Nz`*`Nz`
-/// * A measurement Matrix `H` with dimensions `Nz`*`Nx`
-/// * An initial state `x0` with dimensions `Nx`
-/// * A initial state covariance Matrix `P0` with dimensions `Nx`*`Nx`
-///
-/// ## Usage
-/// Let's make a (terribly uninteresting) instance of the Kalman Filter. We use mostly identity matrices here, for the sake of exemplification.
-/// Replace these matrices with something more interesting. Note that the notation and names used for the Kalman Filter
-/// is similar to the one in the [Wikipedia artice about the Kalman Filter](https://en.wikipedia.org/wiki/Kalman_filter) if you're in doubt which matrix is which
-///
-/// ```
-/// use embedded_kalman::KalmanFilter;
-/// use nalgebra::{SMatrix, SVector, Matrix2};
-/// let kf: KalmanFilter<f64, 2, 2, 2, _> = KalmanFilter::new(
-///     SMatrix::identity(), // A
-///     SMatrix::identity(), // B
-///     SMatrix::identity(), // H
-///     Matrix2::identity().try_into().expect("Q is not a valid covariance Matrix"),
-///     Matrix2::identity().try_into().expect("R is not a valid covariance Matrix"),
-///     SVector::zeros(),
-///     Matrix2::identity().try_into().expect("P0 is not a valid covariance Matrix"),
-/// );
-/// ```
-/// **embedded-kalman** can infer the dimensions of the matrices in the parameters in most cases.
-/// If the dimensions of the matrices do not match, the Kalman Filter won't compile. Note the use of the
-/// `try_into` method. `Q`, `R` and `P0` are required to be valid covariance matrices.
-/// **embedded-kalman** exposes its own [`CovarianceMatrix`] type, which implements [`TryFrom`] for
-/// any square matrix.
-///
-/// Two main methods are used to iterate the KalmanFilter: [update](KalmanFilter::update) and [predict](KalmanFilter::predict).
-///
-/// ```
-/// use embedded_kalman::KalmanFilter;
-/// use nalgebra::Vector2;
-/// let kf = kf
-///     .predict(Vector2::new(1.0, 0.0)) // Give an input to the system
-///     .update(Vector2::new(1.0, 0.0)) // Update the state from a given measurement
-///     .expect("Innovation matrix cannot be inverted");
-/// ```
-/// If the Kalman Filter is in the `Update` state, only the [predict](KalmanFilter::predict) method is available.
-/// If the filter is in the `Predict` state, only the [update](KalmanFilter::update) method is available, preserving
-/// the 2-step recurrent nature of the Kalman Filter.
-///
-/// The update method is failable and might return [None](Option::None) if the innovation matrix is
-/// non-invertible. This happens due to rounding errors when the process noise covariance `Q` is small,
-/// and happens very rarely.
-/// The prior state and state covariance can be fetched after the `Predict` stage.
-/// The posterior state and state covariance can be fetched after the `Update` stage.
-///
-/// ```
-/// use embedded_kalman::KalmanFilter;
-/// use nalgebra::Vector2;
-///
-/// let kf = kf.predict(Vector2::new(1.0, 0.0)); // Give an input to the system
-/// let (x_prior, P_prior) = kf.get_priors(); // Get the priors
-/// let kf = kf
-///     .update(Vector2::new(1.0, 0.0)) // Update the state from a given measurement
-///     .expect("Innovation matrix cannot be inverted");
-/// let (x_posterior, P_posterior) = kf.get_posteriors(); // Get the posteriors
-/// ```
+trait ExtendedKalmanFilterFunctions<
+    T: RealField,       // numerical type use for computation. Typically f32 or f64
+    const Nx: usize,    // Size of the state vector x
+    const Nz: usize,    // Size of the measurement vector z
+    const Nu: usize,    // Size of the input vector u
+> {
+    fn f(x: SVector<T, Nx>, u: SVector<T, Nu>) -> SVector<T, Nx>;
+    fn h(x: SVector<T, Nx>) -> SVector<T, Nz>;
+}
+
 
 /*
 #[derive(Debug)]
 pub struct KalmanFilter
 <
-    T,                  // numerical type use for computation. Typically f32 or f64
+    T: RealField,       // numerical type use for computation. Typically f32 or f64
     const Nx: usize,    // Size of the state vector x
     const Nz: usize,    // Size of the measurement vector z
     const Nu: usize,    // Size of the input vector u
-    S                   // The state of the Kalman Filter
->
-    where
-        T: RealField,
-        S: State,
-{
-    constants: Constants<T, Nx, Nz, Nu>,
+
+    // Non-linear state function and measurement function
+    F: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SVector<T, Nx>,
+    H: Fn(SVector<T, Nx>) -> SVector<T, Nz>,
+
+    // State and measurement jacobians
+    F_: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SMatrix<T, Nx, Nx>,
+    H_: Fn(SVector<T, Nx>) -> SMatrix<T, Nz, Nz>,
+
+    // The state of the Kalman Filter
+    S: State
+> {
+    constants: Constants<T, Nx, Nz, Nu, F, H, F_, H_>,
     state: S,
 }
+
 
 impl<T, const Nx: usize, const Nz: usize, const Nu: usize>
 KalmanFilter<T, Nx, Nz, Nu, Update<T, Nx, Nx>>
@@ -194,25 +136,30 @@ KalmanFilter<T, Nx, Nz, Nu, Predict<T, Nx, Nx>>
 
 */
 
-
 #[derive(Debug)]
-struct Constants<T, const Nx: usize, const Nz: usize, const Nu: usize, Fs, Fj, Hs, Hj>
-    where
+struct Constants<
     T: RealField,
-    Fs: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SVector<T, Nx>,
-    Fj: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SMatrix<T, Nx, Nx>,
-    Hs: Fn(SVector<T, Nx>) -> SVector<T, Nz>,
-    Hj: Fn(SVector<T, Nx>) -> SMatrix<T, Nz, Nz>,
-{
 
-    // State transfer function
-    f: Fs,
-    // Measurement function
-    h: Hs,
+    // Matrix Dimensions
+    const Nx: usize,
+    const Nz: usize,
+    const Nu: usize,
 
-    // Jacobians
-    F: Fj,
-    H: Hj,
+    // Non-linear state function and measurement function
+    F: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SVector<T, Nx>,
+    H: Fn(SVector<T, Nx>) -> SVector<T, Nz>,
+
+    // State and measurement jacobians
+    F_: Fn(SVector<T, Nx>, SVector<T, Nu>) -> SMatrix<T, Nx, Nx>,
+    H_: Fn(SVector<T, Nx>) -> SMatrix<T, Nz, Nz>,
+> {
+    // Non-linear state function and measurement function
+    f: F,
+    h: H,
+
+    // State and measurement jacobians
+    F: F_,
+    H: H_,
 
     // State transition noise matrix
     Q: SMatrix<T, Nx, Nx>,
